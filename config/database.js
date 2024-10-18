98195be292bbae15294a347f115a4af805358395
Isaiah303 <ifrancisco@gbox.ncf.edu.ph>
2024-10-18 20:18:33 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,17 @@
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
+const mysql = require('mysql2');
 
-dotenv.config();
-
-const pool =mysql.createPool({
-   host: process.env.DB_HOST,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME,
-   port: process.env.DB_PORT
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
-module.exports = pool; 
+connection.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err);
+        return;
+    }
+    console.log('Connected to the database.');
+});
